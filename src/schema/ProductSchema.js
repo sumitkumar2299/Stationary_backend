96e476f7a548e9cf439cd43eb98fc6fe1c9db679
sumitkumar2299 <sumitofficialme@gmail.com>
2025-04-11 16:18:42 +0530

    product schema done

---
+++ src/schema/ProductSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    productName:{
+        type:String,
+        required:[true,"product name is required"],
+        minLength:[5,"product name must be atleast 5 characters"],
+        trim:true
+    },
+
+    description:{
+        type:String,
+        minLength:[5,"product description must be atleast 5 characters"],
+    },
+    productImage:{
+        type:String,
+    },
+    price:{
+        type:Number,
+        required:[true,'product price is required']
+    },
+    category:{
+        type:Boolean,
+        required:[true,"in stock status is required"],
+        default:true
+    }
+},{
+    timestamps:true
+})
+
+const Product = mongoose.model('Product',productSchema);
+module.exports = Product;
